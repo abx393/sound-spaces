0b13c3ab6ecaaf350adbdc7754923c32c46a8865
abx393 <abandari99@gmail.com>
2025-04-28 12:05:00 -0500

    updates

--- examples/test.py
+++ examples/test.py
@@ -25,20 +25,23 @@ import torch.nn.functional as F
 import sys
 
 from pathlib import Path
+
 sys.path.append("/content/Spatial_AST")
 import spatial_ast
 
+
 def normalize_audio(audio_data, target_dBFS=-14.0):
-    rms = np.sqrt(np.mean(audio_data**2)) # Calculate the RMS of the audio
-   
+    rms = np.sqrt(np.mean(audio_data ** 2))  # Calculate the RMS of the audio
+
     if rms == 0:  # Avoid division by zero in case of a completely silent audio
         return audio_data
-    
-    current_dBFS = 20 * np.log10(rms) # Convert RMS to dBFS
-    gain_dB = target_dBFS - current_dBFS # Calculate the required gain in dB
-    gain_linear = 10 ** (gain_dB / 20) # Convert gain from dB to linear scale
-    normalized_audio = audio_data * gain_linear # Apply the gain to the audio data
+
+    current_dBFS = 20 * np.log10(rms)  # Convert RMS to dBFS
+    gain_dB = target_dBFS - current_dBFS  # Calculate the required gain in dB
+    gain_linear = 10 ** (gain_dB / 20)  # Convert gain from dB to linear scale
+    normalized_audio = audio_data * gain_linear  # Apply the gain to the audio data
     return normalized_audio
+
 
 def run_spatial_ast(ir, scene_id, setup_id):
     print('reverb padding start')
@@ -54,7 +57,7 @@ def run_spatial_ast(ir, scene_id, setup_id):
     normalize = True
     waveform, sr = sf.read('/content/drive/MyDrive/speech_navigation/follow_me.wav')
     waveform = waveform[:, 0] if len(waveform.shape) > 1 else waveform
-    waveform = signal.resample_poly(waveform, 32000, sr) if sr != 32000 else waveform   
+    waveform = signal.resample_poly(waveform, 32000, sr) if sr != 32000 else waveform
     waveform = normalize_audio(waveform, -14.0) if normalize else waveform
 
     waveform = torch.from_numpy(waveform).reshape(1, -1).float()
@@ -86,14 +89,13 @@ def run_spatial_ast(ir, scene_id, setup_id):
     azimuth_logits = output[2].detach().cpu().numpy()
     elevation_logits = output[3].detach().cpu().numpy()
 
-
     agent_pos = agent.get_state().position
     print('agent_pos', agent_pos)
     print('source_pos', source_pos)
 
-    dx = source_pos[0] - agent_pos[0] # LEFT-RIGHT
-    dy = source_pos[1] - (agent_pos[1] + 1.5) # UP-DOWN
-    dz = source_pos[2] - agent_pos[2] # FRONT-BACK
+    dx = source_pos[0] - agent_pos[0]  # LEFT-RIGHT
+    dy = source_pos[1] - (agent_pos[1] + 1.5)  # UP-DOWN
+    dz = source_pos[2] - agent_pos[2]  # FRONT-BACK
 
     distance_gt = np.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
     print('distance GT', distance_gt)
@@ -105,28 +107,33 @@ def run_spatial_ast(ir, scene_id, setup_id):
     azimuth_gt = (round(azimuth_gt) + 360) % 360
     print('azimuth GT', azimuth_gt)
     elevation_pred = np.argmax(elevation_logits, axis=1)[0]
+    elevation_pred = 25
     print('elevation prediction', elevation_pred)
 
     plt.clf()
     plt.title('Sound Distance Estimation')
-    plt.bar(np.arange(len(distance_logits[0])), distance_logits[0])
+    plt.bar(np.arange(0, len(distance_logits[0]) / 2, 0.5), distance_logits[0], width=0.5)
     plt.plot(distance_gt, np.max(distance_logits[0]) * 1.5, 'go', markersize=15)
-    plt.legend(['Distance Prediction Logits', 'Ground Truth Distance'])
+    plt.legend(['Ground Truth Distance', 'Distance Prediction Logits'])
+    plt.xlabel('Distance (m)')
     plt.savefig(os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id, 'distance_logits'))
 
     plt.clf()
     plt.title('Sound Azimuth Estimation')
     plt.bar(np.arange(len(azimuth_logits[0])), azimuth_logits[0])
-    plt.plot(distance_gt, np.max(distance_logits[0]) * 1.5, 'go', markersize=10)
-    plt.legend(['Azimuth Prediction Logits', 'Ground Truth Azimuth'])
+    plt.plot(azimuth_gt, np.max(azimuth_logits[0]) * 1.5, 'go', markersize=10)
+    plt.legend(['Ground Truth Azimuth', 'Azimuth Prediction Logits'])
+    plt.xlabel('Angle (Degrees)')
     plt.savefig(os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id, 'azimuth_logits'))
 
     plt.clf()
     plt.title('Sound Elevation Estimation')
     plt.bar(np.arange(len(elevation_logits[0])), elevation_logits[0])
+    plt.xlabel('Angle (Degrees)')
     plt.savefig(os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id, 'elevation_logits'))
 
-    return dist_pred, elevation_pred, azimuth_pred
+    return dist_pred, elevation_pred, azimuth_pred, distance_gt, azimuth_gt
+
 
 def get_shortest_path(sim, agent, goal_pos):
     path = habitat_sim.ShortestPath()
@@ -137,21 +144,21 @@ def get_shortest_path(sim, agent, goal_pos):
         print('path points', path.points)
 
     follower = habitat_sim.nav.GreedyGeodesicFollower(
-            sim.pathfinder,
-            agent,
-            goal_radius=0.25
-            #stop_key='stop',
-            #forward_key='move_forward',
-            #left_key='left_key',
-            #right_key='right_key'
+        sim.pathfinder,
+        agent,
+        goal_radius=0.25
+        # stop_key='stop',
+        # forward_key='move_forward',
+        # left_key='left_key',
+        # right_key='right_key'
     )
 
-    #print(follower.find_path(goal_pos))
+    # print(follower.find_path(goal_pos))
     path_points = []
     cnt = 0
     while cnt < 50:
         path_points.append(agent.get_state().position)
-        try: 
+        try:
             next_action = follower.next_action_along(goal_pos)
             if next_action is None:
                 break
@@ -163,11 +170,13 @@ def get_shortest_path(sim, agent, goal_pos):
             break
     return path_points
 
+
 def dist_angle_to_cartesian(agent_pos, dist_pred, elevation_pred, azimuth_pred):
     pred_x = agent_pos[0] + dist_pred * math.cos(math.radians(elevation_pred)) * math.cos(math.radians(azimuth_pred))
     pred_y = agent_pos[1] + dist_pred * math.sin(math.radians(elevation_pred))
     pred_z = agent_pos[2] + dist_pred * math.cos(math.radians(elevation_pred)) * -math.sin(math.radians(azimuth_pred))
     return [pred_x, pred_y, pred_z]
+
 
 # convert 3d points to 2d topdown pixel coordinates
 def convert_points_to_topdown_pixel_coordinates(pathfinder, points, meters_per_pixel):
@@ -182,52 +191,55 @@ def convert_points_to_topdown_pixel_coordinates(pathfinder, points, meters_per_p
 
 
 # display a topdown map with matplotlib
-def display_map(topdown_map, out_file, scene_id, setup_id, source_pos=None, agent_pos=None, goal_pos=None, agent_angle=None, path_points=None):
+def display_map(topdown_map, out_file, scene_id, setup_id, source_pos=None, agent_pos=None, goal_pos=None,
+                agent_angle=None, path_points=None):
     plt.figure(figsize=(12, 8))
     ax = plt.subplot(1, 1, 1)
-    #ax.axis("off")
+    ax.axis("off")
     plt.imshow(topdown_map)
     # plot points on map
     if source_pos is not None:
         plt.plot(source_pos[0], source_pos[1], "go", markersize=10, alpha=0.8)
-        plt.legend('Ground Truth Speech Source Position')
-    if agent_pos is not None:
-        plt.plot(agent_pos[0], agent_pos[1], "ro", markersize=10, alpha=0.8)
-        plt.legend('Agent End Position')
     if goal_pos is not None:
         plt.plot(goal_pos[0], goal_pos[1], "bo", markersize=10, alpha=0.8)
-        plt.legend('Estimated Sound Source Position')
+    if agent_pos is not None:
+        plt.plot(agent_pos[0], agent_pos[1], "ro", markersize=10, alpha=0.8)
     if path_points is not None:
-        plt.plot([point[0] for point in path_points], [point[1] for point in path_points], "ro", markersize=5, alpha=0.8)
-        plt.legend('Agent Steps')
+        plt.plot([point[0] for point in path_points], [point[1] for point in path_points], "ro", markersize=5,
+                 alpha=0.8)
+
+    plt.legend(['GT Speech Source Position', 'Estimated Sound Source Position', 'Agent End Position', 'Agent Steps'])
 
     if agent_angle is not None:
         arrow_len = 50
-        arrow_dest = (agent_pos[0] + arrow_len * math.sin(agent_angle), agent_pos[1] - arrow_len * math.cos(agent_angle))
+        arrow_dest = (
+        agent_pos[0] - arrow_len * math.sin(agent_angle), agent_pos[1] - arrow_len * math.cos(agent_angle))
         ax.annotate('', xytext=agent_pos, xy=arrow_dest, arrowprops=dict(arrowstyle="->"))
 
+    # plt.show(block=False)
+    # plt.set_xticks([1,2,3,4])
+    # plt.tight_layout()
+    plt.savefig(os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id, out_file),
+                bbox_inches='tight')
 
-    #plt.show(block=False)
-    #plt.set_xticks([1,2,3,4])
-    plt.savefig(os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id, out_file), bbox_inches='tight')
 
 def quaternion_to_axis_angle(quat):
     q_array = np.array([quat.x, quat.y, quat.z, quat.w])
     rotation = R.from_quat(q_array)
-    
+
     # Get the axis-angle representation
     axis_angle = rotation.as_rotvec()
-    
+
     # Calculate the angle from the magnitude of the rotation vector
-    angle = np.linalg.norm(axis_angle)
-    
+    angle = np.sign(axis_angle[1]) * np.linalg.norm(axis_angle)
+
     # Normalize the rotation vector to get the axis
     if angle != 0:
         axis = axis_angle / angle
     else:
-        axis = np.array([1.0, 0.0, 0.0])  # Handle zero rotation case
+        axis = np.array([0.0, 1.0, 0.0])  # Handle zero rotation case
 
-    return axis, -angle
+    return axis, angle
 
 
 device = torch.device('cuda')
@@ -262,10 +274,13 @@ sim.add_sensor(audio_sensor_spec)
 audio_sensor = sim.get_agent(0)._sensors["audio_sensor"]
 audio_sensor.setAudioMaterialsJSON("data/mp3d_material_config.json")
 
-#seed = random.randint(0, 100)
-#print('seed', seed)
-#sim.seed(seed)
+# seed = random.randint(0, 100)
+# print('seed', seed)
+# sim.seed(seed)
 
+azimuth_in_range = 0
+distance_in_range = 0
+total_cnt = 0
 # set navmesh path for searching for navigable points
 sim.pathfinder.load_nav_mesh(os.path.join(f"data/scene_datasets/mp3d/{scene_id}/{scene_id}.navmesh"))
 
@@ -274,12 +289,14 @@ for scene_config in reverb_config['data']:
     if not scene_config['fname'].startswith(scene_id):
         continue
 
+    total_cnt += 1
+
     setup_id = scene_config['fname'].split('/')[-1].split('.')[0]
     print('scene_id', scene_id)
     print('setup_id', setup_id)
     os.makedirs(os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id), exist_ok=True)
 
-    #try:
+    # try:
     fname = f'/content/drive/MyDrive/mp3d_reverb/binaural/{scene_config["fname"]}'
     agent_pos = [float(val) for val in scene_config['agent_position'].split(',')]
     sensor_pos = [float(val) for val in scene_config['sensor_position'].split(',')]
@@ -292,24 +309,23 @@ for scene_config in reverb_config['data']:
     ir_precomputed = np.load(fname)
     print('ir_precomputed.shape', ir_precomputed.shape)
 
-    #source_pos = np.array([-7.9259,1.52,-2.8804])
-    #source_pos = np.array([-8.2144,1.1012,-4.3399])
-    #source_pos = np.array([-9.3071,1.9991,-2.8131])
-    #source_pos = np.array([-3.4029,0.9901,-0.1906])
+    # source_pos = np.array([-7.9259,1.52,-2.8804])
+    # source_pos = np.array([-8.2144,1.1012,-4.3399])
+    # source_pos = np.array([-9.3071,1.9991,-2.8131])
+    # source_pos = np.array([-3.4029,0.9901,-0.1906])
 
     audio_sensor.setAudioSourceTransform(source_pos)
     agent = sim.get_agent(0)
     new_state = sim.get_agent(0).get_state()
     new_state.position = np.array(agent_pos)
-    #new_state.position = sim.pathfinder.get_random_navigable_point_near(source_pos, 6)
-    #new_state.position = np.array([-7.2797,0.0724,-2.0944])
-    #new_state.position = np.array([-7.5873,0.0724,-1.7346])
-    #new_state.position = np.array([-4.0593,0.0724,-0.311])
+    # new_state.position = sim.pathfinder.get_random_navigable_point_near(source_pos, 6)
+    # new_state.position = np.array([-7.2797,0.0724,-2.0944])
+    # new_state.position = np.array([-7.5873,0.0724,-1.7346])
+    # new_state.position = np.array([-4.0593,0.0724,-0.311])
 
     print('agent position', new_state.position)
     new_state.sensor_states = {}
     agent.set_state(new_state, True)
-
 
     ir = np.array(sim.get_sensor_observations()["audio_sensor"])
     print('ir shape', ir.shape)
@@ -317,7 +333,7 @@ for scene_config in reverb_config['data']:
     # check if the direct sound is present (source is visibile from the listener)
     print('source is visble', audio_sensor.sourceIsVisible())
 
-    # check the efficiency of rendering, outdoor would have a very low value, e.g. < 0.05, 
+    # check the efficiency of rendering, outdoor would have a very low value, e.g. < 0.05,
     # while a closed indoor room would have >0.95, and a room with some holes might be in the 0.1-0.8 range.
     # if the ray efficiency is low for an indoor environment, it indicates a lot of ray leak from holes
     # you should repair the mesh in this case for more accurate acoustic rendering
@@ -329,14 +345,16 @@ for scene_config in reverb_config['data']:
     plt.title('Impulse Response')
     for i in range(2):
         plt.plot(np.linspace(0, samples_clip / sr, samples_clip), ir[i, :samples_clip])
-        plt.savefig(os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id, 'ir{}'.format(i)))
+        plt.savefig(
+            os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id, 'ir{}'.format(i)))
 
     plt.clf()
 
     plt.title('Impulse Response Precomputed')
     for i in range(2):
         plt.plot(np.linspace(0, samples_clip / sr, samples_clip), ir_precomputed[i, :samples_clip])
-        plt.savefig(os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id, 'ir_precomputed{}'.format(i)))
+        plt.savefig(os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id,
+                                 'ir_precomputed{}'.format(i)))
 
     plt.clf()
     ir = ir_precomputed
@@ -351,9 +369,8 @@ for scene_config in reverb_config['data']:
 
     plt.title('Original Speech')
     plt.plot(np.linspace(0, len(vocal) / sr, len(vocal)), vocal)
-    #plt.savefig(os.path.join('/content', 'output', 'original_speech'))
+    # plt.savefig(os.path.join('/content', 'output', 'original_speech'))
     plt.clf()
-
 
     # convolve the vocal with IR
     convolved_vocal = np.array([fftconvolve(vocal, ir_channel) for ir_channel in ir])
@@ -361,13 +378,16 @@ for scene_config in reverb_config['data']:
     plt.title('Speech convolved with IR')
     for i in range(2):
         plt.plot(np.linspace(0, len(convolved_vocal[i]) / sr, len(convolved_vocal[i])), convolved_vocal[i])
-        plt.savefig(os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id, 'speech_convolved_with_ir{}'.format(i)))
+        plt.savefig(os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id,
+                                 'speech_convolved_with_ir{}'.format(i)))
     plt.clf()
 
-    #convolved_vocal = convolved_vocal / np.expand_dims(np.max(convolved_vocal, axis=1), axis=1)
+    # convolved_vocal = convolved_vocal / np.expand_dims(np.max(convolved_vocal, axis=1), axis=1)
     iinfo = np.iinfo(np.int16)
     convolved_vocal = (convolved_vocal / np.max(np.abs(convolved_vocal)) * iinfo.max).astype(np.int16)
-    wavfile.write(os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id, 'reverberent_speech.wav'), sr, convolved_vocal.T)
+    wavfile.write(
+        os.path.join('/content/drive/MyDrive/spoken_navigation_output', scene_id, setup_id, 'reverberent_speech.wav'),
+        sr, convolved_vocal.T)
 
     rt60 = measure_rt60(ir[0], sr, decay_db=30, plot=True)
     print(f'RT60 of the rendered IR is {rt60:.4f} seconds')
@@ -375,8 +395,14 @@ for scene_config in reverb_config['data']:
     action_names = list(cfg.agents[0].action_space.keys())
     print('action names', action_names)
 
-
-    dist_pred, elevation_pred, azimuth_pred = run_spatial_ast(ir, scene_id, setup_id)
+    dist_pred, elevation_pred, azimuth_pred, dist_gt, azimuth_gt = run_spatial_ast(ir, scene_id, setup_id)
+    if abs(azimuth_gt - azimuth_pred) <= 20:
+        azimuth_in_range += 1
+    if abs(dist_gt - dist_pred) <= 1:
+        distance_in_range += 1
+    print('azimuth_in_range', azimuth_in_range)
+    print('distance_in_range', distance_in_range)
+    print('total count', total_cnt)
 
     agent_pos = agent.get_state().position
     goal_pos = dist_angle_to_cartesian(agent_pos, dist_pred, elevation_pred, azimuth_pred)
@@ -384,7 +410,6 @@ for scene_config in reverb_config['data']:
 
     print('Finding shortest geodesic path...')
     path_points = get_shortest_path(sim, agent, goal_pos)
-
 
     meters_per_pixel = 0.01  # @param {type:"slider", min:0.01, max:1.0, step:0.01}
     # @markdown Customize the map slice height (global y coordinate):
@@ -397,11 +422,10 @@ for scene_config in reverb_config['data']:
         # get bounding box minimum elevation for automatic height
         height = sim.pathfinder.get_bounds()[0][1]
 
-
     # @markdown You can get the topdown map directly from the Habitat-sim API with *PathFinder.get_topdown_view*.
     # This map is a 2D boolean array
     sim_topdown_map = sim.pathfinder.get_topdown_view(meters_per_pixel, height)
-    
+
     # @markdown Alternatively, you can process the map using the Habitat-Lab [maps module](https://github.com/facebookresearch/habitat-lab/blob/main/habitat/utils/visualizations/maps.py)
     hablab_topdown_map = maps.get_topdown_map(
         sim.pathfinder, height, meters_per_pixel=meters_per_pixel
@@ -423,10 +447,11 @@ for scene_config in reverb_config['data']:
     print('rot_angle degrees ', math.degrees(rot_angle))
     print('source and agent pixel positions', pos_pixels)
 
-    #print("Displaying the raw map from get_topdown_view:")
-    #display_map(sim_topdown_map, 'habitat_sim_get_topdown_view', scene_id, setup_id, source_pos=pos_pixels[-1], goal_pos=pos_pixels[-2], agent_pos=pos_pixels[-3], agent_angle=rot_angle, path_points=pos_pixels[:-3])
+    # print("Displaying the raw map from get_topdown_view:")
+    # display_map(sim_topdown_map, 'habitat_sim_get_topdown_view', scene_id, setup_id, source_pos=pos_pixels[-1], goal_pos=pos_pixels[-2], agent_pos=pos_pixels[-3], agent_angle=rot_angle, path_points=pos_pixels[:-3])
     print("Displaying the map from the Habitat-Lab maps module:")
-    display_map(hablab_topdown_map, 'habitat_lab_get_topdown_map', scene_id, setup_id, source_pos=pos_pixels[-1], goal_pos=pos_pixels[-2], agent_pos=pos_pixels[-3], agent_angle=rot_angle, path_points=pos_pixels[:-3])
+    display_map(hablab_topdown_map, 'habitat_lab_get_topdown_map', scene_id, setup_id, source_pos=pos_pixels[-1],
+                goal_pos=pos_pixels[-2], agent_pos=pos_pixels[-3], agent_angle=rot_angle, path_points=pos_pixels[:-3])
 
-    #except:
+    # except:
     #    print('error loading config', scene_config['fname'])
